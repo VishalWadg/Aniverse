956c395a34b6838f6de86139f5a935194113b100
Vishal Vivek Wadgaonkar <wadgaonkarvishal54@gmail.com>
2025-10-27 10:41:09 +0530

    initial commit

---
+++ Backend/src/main/java/com/vvw/AniverseBackend/AniverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vvw.AniverseBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniverseBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/vvw/AniverseBackend/AniverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvw.AniverseBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
