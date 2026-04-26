185bf2b49bf40381ebfbde0cdbd2f3c61ae6100b
Vishal Vivek Wadgaonkar <wadgaonkarvishal54@gmail.com>
2026-04-26 09:59:47 +0530

    review suggestions

--- Backend/src/main/java/com/vvw/AniverseBackend/controller/UrlMetadataController.java
+++ Backend/src/main/java/com/vvw/AniverseBackend/controller/UrlMetadataController.java
@@ -1,22 +1,22 @@
-package com.vvw.AniverseBackend.controller;
+// package com.vvw.AniverseBackend.controller;
 
-import com.vvw.AniverseBackend.dto.UrlMetadataResponse;
-import com.vvw.AniverseBackend.service.UrlMetadataService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import com.vvw.AniverseBackend.dto.UrlMetadataResponse;
+// import com.vvw.AniverseBackend.service.UrlMetadataService;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/url-meta")
-public class UrlMetadataController {
-    private final UrlMetadataService urlMetadataService;
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping("/url-meta")
+// public class UrlMetadataController {
+//     private final UrlMetadataService urlMetadataService;
 
-    @GetMapping
-    public ResponseEntity<UrlMetadataResponse> fetchMetadata(@RequestParam("url") String url) {
-        return ResponseEntity.ok(urlMetadataService.fetch(url));
-    }
-}
+//     @GetMapping
+//     public ResponseEntity<UrlMetadataResponse> fetchMetadata(@RequestParam("url") String url) {
+//         return ResponseEntity.ok(urlMetadataService.fetch(url));
+//     }
+// }
