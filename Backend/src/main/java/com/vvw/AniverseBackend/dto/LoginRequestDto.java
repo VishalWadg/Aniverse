b1214ebaf67b8e363ef9fe4a6169b145db11d04a
Vishal Vivek Wadgaonkar <wadgaonkarvishal54@gmail.com>
2026-06-11 08:58:51 +0530

    fixed auth validation mismatch between BE and FE

--- Backend/src/main/java/com/vvw/AniverseBackend/dto/LoginRequestDto.java
+++ Backend/src/main/java/com/vvw/AniverseBackend/dto/LoginRequestDto.java
@@ -2,6 +2,7 @@ package com.vvw.AniverseBackend.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
-    @NotBlank
-    @NotNull
+    @NotBlank(message = "Username is required")
+    @Size(min=3, max=30, message="Username should be 3-30 characters long")
     private String username;
-    @NotBlank
-    @NotNull
+
+    @NotBlank(message = "Password is required")
+    @Size(min=8, message="Password must be at least 8 characters")
     private String password;
 }
