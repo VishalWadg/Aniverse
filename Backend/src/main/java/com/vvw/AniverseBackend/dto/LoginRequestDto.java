fc2cd18565651bc87b1478475e62f062b98445c2
Vishal Vivek Wadgaonkar <wadgaonkarvishal54@gmail.com>
2026-06-11 09:05:13 +0530

    fixed removed size validation from logindto

--- Backend/src/main/java/com/vvw/AniverseBackend/dto/LoginRequestDto.java
+++ Backend/src/main/java/com/vvw/AniverseBackend/dto/LoginRequestDto.java
@@ -12,10 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginRequestDto {
     @NotBlank(message = "Username is required")
-    @Size(min=3, max=30, message="Username should be 3-30 characters long")
     private String username;
 
     @NotBlank(message = "Password is required")
-    @Size(min=8, message="Password must be at least 8 characters")
     private String password;
 }
